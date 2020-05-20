69d3b30523a4e37cb1172d399a8e5580a9960f6b
Joe Di Pol <joe.dipol@oracle.com>
2020-05-20 10:53:46 -0700

    Add Helidon MP example

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 This repository contains the following examples that show how to deploy 
 Helidon applications to Google App Engine
 
-1. [helidon-quickstart-se](./helidon-quickstart-se): The Helidon SE quickstart
-   applications.
-
+1. [helidon-quickstart-se](./helidon-quickstart-se): The Helidon SE quickstart 
+   application.
+2. [helidon-quickstart-mp](./helidon-quickstart-mp): The Helidon MP quickstart
+   application.
---
+++ helidon-quickstart-mp/src/main/java/io/helidon/examples/quickstart/mp/Main.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2018, 2020 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.examples.quickstart.mp;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.LogManager;
+
+import io.helidon.microprofile.server.Server;
+
+/**
+ * The application main class.
+ */
+public final class Main {
+
+    /**
+     * Cannot be instantiated.
+     */
+    private Main() {
+    }
+
+    /**
+     * Application main entry point.
+     * @param args command line arguments
+     * @throws IOException if there are problems reading logging properties
+     */
+    public static void main(final String[] args) throws IOException {
+
+        int port = 0;
+
+        // load logging configuration
+        setupLogging();
+
+        // Override port with the PORT environment variable that is set by Google App Engine
+        String envPort = System.getenv().getOrDefault("PORT", "");
+        if (! envPort.isEmpty()) {
+            port = Integer.parseInt(envPort);
+        }
+
+        // start the server
+        Server server = startServer(port);
+
+        System.out.println("http://localhost:" + server.port() + "/greet");
+    }
+
+    /**
+     * Start the server.
+     * @return the created {@link Server} instance
+     */
+    static Server startServer(int port) {
+        // Server will automatically pick up configuration from
+        // microprofile-config.properties
+        // and Application classes annotated as @ApplicationScoped
+
+        Server.Builder builder = Server.builder();
+        if (port > 0) {
+            builder.port(port);
+        }
+        return builder.build().start();
+   }
+
+    /**
+     * Configure logging from logging.properties file.
+     */
+    private static void setupLogging() throws IOException {
+        try (InputStream is = Main.class.getResourceAsStream("/logging.properties")) {
+            LogManager.getLogManager().readConfiguration(is);
+        }
+    }
+}
